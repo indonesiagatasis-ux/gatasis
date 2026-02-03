7f39c3c72c2640a9f9175cee8125344392e6b8e9
indonesiagatasis-ux <indonesiagatasis@gmail.com>
2026-02-03 15:40:01 +0700

    Add basic and database configuration in config.php

---
+++ admin/layouts/config.php
@@ -0,0 +1,21 @@
+<?php
+// ==============================
+// BASIC CONFIG
+// ==============================
+
+define('BASE_URL', 'http://localhost/gatasis/admin/');
+
+// ==============================
+// DATABASE CONFIG
+// ==============================
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'cms_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// ==============================
+// TIMEZONE
+// ==============================
+
+date_default_timezone_set('Asia/Jakarta');
